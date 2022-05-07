24e54c01679f8036df3f0776e6832a8e7e172060
dainiuxt <dainiuxt@gmail.com>
2022-05-07 11:45:19 +0300

    remove insecure django key

--- orderstask/settings.py
+++ orderstask/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+5bn_@ga3ssm$wu6g9lb9@n+^9i^2z2@g7y22r(500kx8-)668'
+# SECRET_KEY = 'django-insecure-+5bn_@ga3ssm$wu6g9lb9@n+^9i^2z2@g7y22r(500kx8-)668'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
